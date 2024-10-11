ad938524d42841455260319739bc6554c84f221c
bina0808 <sabrina.domaredzki@escola.pr.gov.br>
2024-10-11 09:35:51 -0300

    dfgh

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'Drugs',
+    'O que são drogas?',
+    'Substâncias que alteram funções físicas ou psicológicas.'
+)
+criaCartao(
+    'Drugs',
+    'Quais são os tipos principais de drogas?',
+    'Licitas (álcool, tabaco) e ilícitas (maconha, cocaína).'
+
+)
+criaCartao(
+    'Drugs',
+    'O que é dependência química?',
+    'Necessidade compulsiva de consumir uma droga'
+
+)
+criaCartao(
+    'Drugs',
+    'Quais são os efeitos da maconha?',
+    'Relaxamento, alteração na percepção e aumento do apetite.'
+
+)
+criaCartao(
+    'Drugs',
+    'O que é crack?',
+    'Forma cristalizada da cocaína, altamente viciante.'
+
+)
+criaCartao(
+    'Drugs',
+    '',
+    ''
+
+)
+criaCartao(
+    'Drugs',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
+criaCartao(
+    '',
+    '',
+    ''
+
+)
